675d215c7c4e17917add00f66cabff9228fd852a
Viktoriia Melnyk <mviktoria1005@gmail.com>
2022-03-25 21:17:47 +0200

    done task

--- src/index.js
+++ src/index.js
@@ -1,1 +1,93 @@
+import './css/common.css';
 
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import ApiService from './js/api-service';
+import markup from './js/markup';
+import LoadMoreBtn from './js/load-more-button';
+
+const form = document.querySelector('.search-form');
+const searchButton = document.querySelector('[type=submit]');
+
+const gallery = document.querySelector('.gallery');
+
+const options = {
+  simpleLightBox: {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+  },
+};
+
+const apiService = new ApiService();
+const loadMoreBtn = new LoadMoreBtn({
+  selector: '.load-more',
+  hidden: true,
+});
+
+let galleryLightBox = new SimpleLightbox('.gallery a', options.simpleLightBox);
+
+function dataProcessing(data) {
+  if (data.data.totalHits === 0) {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+  if (data.data.totalHits !== 0 && data.data.hits.length === 0) {
+    searchButton.disabled = true;
+    return Notify.warning(`We're sorry, but you've reached the end of search results.`);
+  }
+
+  gallery.insertAdjacentHTML('beforeend', markup(data.data.hits));
+
+  galleryLightBox.refresh();
+
+  if (apiService.pageNumber === 2) {
+    return Notify.info(`Hooray! We found ${data.data.totalHits} images.`);
+  } else {
+    const { height: cardHeight } = gallery.firstElementChild.getBoundingClientRect();
+    window.scrollBy({
+      top: cardHeight * 2 + 120,
+      behavior: 'smooth',
+    });
+  }
+}
+
+function loadPictures() {
+  apiService
+    .getPictures()
+    .then(dataProcessing)
+    .then(loadMoreBtn.show())
+    .catch(error => {
+      console.log(error);
+      Notify.failure('Something went wrong, please try again...');
+    });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const isFilled = event.currentTarget.elements.searchQuery.value;
+  if (isFilled) {
+    searchButton.disabled = true;
+    apiService.searchQuery = isFilled;
+    apiService.resetPage();
+    gallery.innerHTML = '';
+    loadPictures();
+  } else {
+    return Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.',
+    );
+  }
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onLoadMore() {
+  loadPictures();
+}
+
+loadMoreBtn.refs.button.addEventListener('click', onLoadMore);
---
+++ src/js/api-service.js
@@ -0,0 +1,47 @@
+import axios from 'axios';
+
+export default class ApiService {
+  constructor() {
+    this.options = {
+      params: {
+        key: '26225007-03edf0f89f8f7aca6e8b387a9',
+        q: '',
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page: 1,
+        per_page: 40,
+      },
+    };
+  }
+
+  incrementPage() {
+    this.options.params.page += 1;
+  }
+
+  async getPictures() {
+    const response = await axios.get('https://pixabay.com/api/', this.options);
+    this.incrementPage();
+    return response;
+  }
+
+  resetPage() {
+    this.pageNumber = 1;
+  }
+
+  get searchQuery() {
+    return this.options.params.q;
+  }
+
+  set searchQuery(newQuery) {
+    this.options.params.q = newQuery;
+  }
+
+  get pageNumber() {
+    return this.options.params.page;
+  }
+
+  set pageNumber(newNumber) {
+    this.options.params.page = newNumber;
+  }
+}
